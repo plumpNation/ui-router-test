f2bdf4d041b95eb200ad28d6b3b32114a753286d
Gavin King <gavin.king@itslearning.com>
2015-12-15 17:28:49 +0100

    Popping in controllerAs

--- app/main.js
+++ app/main.js
@@ -1,3 +1,11 @@
+var plans = [
+    {'title': 'Plan 1', 'description': 'A detailed description of 1'},
+    {'title': 'Plan 2', 'description': 'A detailed description of 2'},
+    {'title': 'Plan 3', 'description': 'A detailed description of 3'},
+    {'title': 'Plan 4', 'description': 'A detailed description of 4'},
+    {'title': 'Plan 5', 'description': 'A detailed description of 5'}
+];
+
 var myApp = angular.module('myApp', ['ui.router'])
     .config(function ($stateProvider, $urlRouterProvider) {
         $urlRouterProvider.otherwise('/plans/focus/0');
@@ -6,35 +14,43 @@ var myApp = angular.module('myApp', ['ui.router'])
             .state('plans', {
                 url: '/plans',
                 templateUrl: 'partials/plans.html',
-                controller: function ($scope, $state, $location) {
-                    $scope.plans = [
-                        'Plan 1',
-                        'Plan 2',
-                        'Plan 3',
-                        'Plan 4',
-                        'Plan 5'
-                    ];
+                controllerAs: 'pvm',
 
-                    $scope.createPlan = function (planName) {
-                        $scope.plans.push(planName);
-                        $state.go('plans.focus', {planId: $scope.plans.length - 1});
+                controller: function ($scope, $state, $location) {
+                    var self = this;
+
+                    self.plans = plans;
+
+                    self.createPlan = function (planName, planDescription) {
+                        self.plans.push({
+                            'title': planName,
+                            'description': planDescription
+                        });
+
+                        $state.go('plans.focus', {
+                            'planId': $scope.plans.length - 1
+                        });
                     };
                 }
             })
             .state('plans.focus', {
                 url: '/focus/:planId',
                 templateUrl: 'partials/plans.focus.html',
-                controller: function ($scope, $stateParams) {
-                    $scope.focusPlan = $scope.plans[$stateParams.planId];
+                controllerAs: 'pfvm',
+
+                controller: function ($stateParams) {
+                    this.planDetail = plans[$stateParams.planId - 1];
                 }
             })
             .state('plans.create', {
                 url: '/create',
                 templateUrl: 'partials/plans.create.html',
+                controllerAs: 'pcvm',
+
                 controller: function ($timeout) {
                     $timeout(function () {
                         document.getElementById('plan-name').focus();
-                    }, 0);
+                    });
                 }
             });
     });
